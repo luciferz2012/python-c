50707461f3aa8b1f9ebb3f43808d45c4ae054703
Luciferz2012 <luciferz2012@gmail.com>
2015-11-19 21:17:23 +0800

    compile ifcengine.dll

--- SWIG/make.bat
+++ SWIG/make.bat
@@ -10,11 +10,11 @@ copy /y ifcengine\data\* *
 swig -python ifc.i
 
 call "%VS%\VC\vcvarsall.bat" amd64
-cl %C2Python%_wrap.c /I"%PyX64%\include" "%PyX64%\libs\%PyLib%" ifcengine\lib64\ifcengine.lib /Fe_%C2Python%.pyd /LD
+cl %C2Python%_wrap.c /I"%PyX64%\include" "%PyX64%\libs\%PyLib%" ifcengine\lib64\ifcengine.lib /Fe_%C2Python%.pyd /LD /D "_WINDOWS"
 copy /y ifcengine\bin64\ifcengine.dll ifcengine.dll
 "%PyX64%\python.exe" runme.py
 
-rem call "%VS%\VC\vcvarsall.bat"
-rem cl %C2Python%_wrap.c /I"%PyX32%\include" "%PyX32%\libs\%PyLib%" ifcengine\lib32\ifcengine.lib /Fe_%C2Python%.pyd /LD
-rem copy /y ifcengine\bin32\ifcengine.dll ifcengine.dll
-rem "%PyX32%\python.exe" runme.py
+call "%VS%\VC\vcvarsall.bat"
+cl %C2Python%_wrap.c /I"%PyX32%\include" "%PyX32%\libs\%PyLib%" ifcengine\lib32\ifcengine.lib /Fe_%C2Python%.pyd /LD /D "_WINDOWS"
+copy /y ifcengine\bin32\ifcengine.dll ifcengine.dll
+"%PyX32%\python.exe" runme.py
